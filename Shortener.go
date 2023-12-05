e26da3a20888f82fcd66584e167c3578fbcaa572
b3prog <151466720+b3prog@users.noreply.github.com>
2023-12-06 03:21:50 +0700

    Shortener.go

--- Shortener.go
+++ Shortener.go
@@ -1,59 +1,90 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"net/url"
+  "fmt"
+  "io/ioutil"
+  "net/http"
+  "net/url"
+  "time"
 )
 
 func isValidURL(rawURL string) bool {
-	parsedURL, err := url.ParseRequestURI(rawURL)
-	if err != nil {
-		return false
-	}
-	return parsedURL.Scheme != "" && parsedURL.Host != ""
+  parsedURL, err := url.ParseRequestURI(rawURL)
+  if err != nil {
+    return false
+  }
+  return parsedURL.Scheme != "" && parsedURL.Host != ""
 }
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodPost {
-			long := r.FormValue("url")
+  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    if r.Method == http.MethodPost {
+      long := r.FormValue("url")
 
-			if !isValidURL(long) {
-				http.Error(w, "Invalid URL", http.StatusBadRequest)
-				return
-			}
+      if !isValidURL(long) {
+        http.Error(w, "Invalid URL", http.StatusBadRequest)
+        return
+      }
 
-			resp, err := http.PostForm("http://localhost:8081/set", url.Values{"url": {long}})
-			if err != nil {
-				http.Error(w, "Error communicating with storage service", http.StatusInternalServerError)
-				return
-			}
-			defer resp.Body.Close()
+      resp, err := http.PostForm("http://localhost:8081/set", url.Values{"url": {long}})
+      if err != nil {
+        http.Error(w, "Error communicating with storage service", http.StatusInternalServerError)
+        return
+      }
+      defer resp.Body.Close()
 
-			if resp.StatusCode == http.StatusOK {
-				fmt.Fprintf(w, "Shortened URL: %s\n", resp.Header.Get("Location"))
-			} else {
-				http.Error(w, "Error creating shortened URL", resp.StatusCode)
-			}
-		} else if r.Method == http.MethodGet {
-			resp, err := http.Get("http://localhost:8081/get" + r.URL.Path)
-			if err != nil {
-				http.Error(w, "Error communicating with storage service", http.StatusInternalServerError)
-				return
-			}
-			defer resp.Body.Close()
+      if resp.StatusCode == http.StatusOK {
+        shortURL := resp.Header.Get("Location")
+        fmt.Fprintf(w, "Shortened URL: %s", shortURL)
 
-			if resp.StatusCode == http.StatusTemporaryRedirect {
-				http.Redirect(w, r, resp.Header.Get("Location"), http.StatusTemporaryRedirect)
-			} else {
-				http.NotFound(w, r)
-			}
-		} else {
-			w.WriteHeader(http.StatusMethodNotAllowed)
-		}
-	})
+      } else if resp.StatusCode == http.StatusConflict {
+        fmt.Fprintf(w, "Shortened URL already exists")
+      } else {
+        http.Error(w, "Error creating shortened URL", resp.StatusCode)
+      }
+    } else if r.Method == http.MethodGet {
+      shortURL := r.URL.Path[1:]
+      if shortURL != "" {
+        // Get user's IP address
+        userIP := r.RemoteAddr
 
-	fmt.Println("URL shortener started on port 8080")
-	http.ListenAndServe(":8080", nil)
+        // Get current timestamp
+        timestamp := time.Now().Format(time.RFC3339)
+
+        // Get long URL
+        resp, err := http.Get("http://localhost:8081/get/" + shortURL)
+        if err != nil {
+          http.Error(w, "Error getting long URL", http.StatusInternalServerError)
+          return
+        }
+        defer resp.Body.Close()
+
+        longURLBytes, err := ioutil.ReadAll(resp.Body)
+        if err != nil {
+          http.Error(w, "Error reading long URL", http.StatusInternalServerError)
+          return
+        }
+
+        longURL := string(longURLBytes)
+
+        // Send data to statistics service
+        _, err = http.PostForm("http://localhost:8082", url.Values{"ip": {userIP}, "url": {shortURL}, "long_url": {longURL}, "timestamp": {timestamp}})
+        if err != nil {
+          http.Error(w, "Error sending data to statistics service", http.StatusInternalServerError)
+          return
+        }
+
+        fmt.Printf("Sent data to statistics service: {SourceIP:%s URL:%s Short:%s Timestamp:%s}\n", userIP, longURL, shortURL, timestamp)
+
+        http.Redirect(w, r, longURL, http.StatusTemporaryRedirect)
+        return
+      }
+      http.NotFound(w, r)
+    } else {
+      w.WriteHeader(http.StatusMethodNotAllowed)
+    }
+  })
+
+  fmt.Println("URL Shortener started on port 8080")
+  http.ListenAndServe(":8080", nil)
 }
